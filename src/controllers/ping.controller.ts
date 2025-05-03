75bcc1826d8c295d9f60c7233d353f1f91bf8e73
yasharora7 <y79arora@gmail.com>
2025-05-03 16:04:23 +0530

    adding error handling

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -1,13 +1,22 @@
- import { Request, Response } from "express";
-
+ import { NextFunction, Request, Response } from "express";
+ import fs from "fs/promises";
+ import { NotFoundError } from "../utils/errors/app.error";
  
 
-  export const pingHandler=(req: Request,res: Response)=>{
-    // console.log(req.body);
-    // res.send("pong");
-    res.status(200).json({
-      message:"Pong",
-      success: true,
-    })
+  // export const pingHandler=(req: Request,res: Response)=>{
+  //   // console.log(req.body);
+  //   // res.send("pong");
+  //   res.status(200).json({
+  //     message:"Pong",
+  //     success: true,
+  //   })
+  //   }
+ 
+  export const pingHandler= async(req: Request, res: Response, next: NextFunction)=> {
+    try{
+      await fs.readFile("sample");
+      res.status(200).json({message:"Pong!"});
+    }catch(error){
+      throw new NotFoundError("File not found!");
     }
- 
+  }
